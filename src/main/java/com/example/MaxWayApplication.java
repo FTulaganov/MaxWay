584012db47274815cbc099096fd2a4135cd2f965
Fathulla <fathullatolaganov134@gmail.com>
2023-10-07 18:54:33 +0500

    init

---
+++ src/main/java/com/example/MaxWayApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaxWayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MaxWayApplication.class, args);
+	}
+
+}
